32de2fb590af7fb66d5ab6b5f50cd7e9eafe10ff
lg0leg <lg.oleg1067@gmail.com>
2022-07-05 23:13:43 +0300

    init: start project

---
+++ webpack.dev.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, './dist'),
+    },
+    compress: true,
+    port: 3000,
+    open: {
+      app: {
+        name: 'chrome',
+      },
+    },
+  },
+};
