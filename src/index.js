18a581677de4b37a7fec9743576247d3f279011f
lg0leg <lg.oleg1067@gmail.com>
2022-07-08 16:24:57 +0300

    refactor: change connection type of fonts

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
+/*css*/
+
 import './assets/css/reset.css';
+import './assets/css/fonts.css';
 import './assets/css/nouislider.css';
 import './assets/css/style.css';
+
+/*js*/
 
 import './assets/js/filters.js';
